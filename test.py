c6477a91913ad1530d0328fb82038dff30b6c99f
unknown <isaimartinezcontreras@gmail.com>
2019-11-11 11:28:10 -0600

    V3

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 #Hough transform for road detection
 import cv2
 import numpy as np
-#img = cv2.imread("rural_highway_0.jpg")
+img = cv2.imread("rural_highway_0.jpg")
 img = cv2.imread("Carretera1.jpg")
-orig = cv2.imread("Carretera1.jpg")
 #img = cv2.imread("Carretera2.jpg")
 #img = cv2.imread("Carretera3.jpg")
+orig = img
+img = cv2.GaussianBlur(img, (5, 5), 0)
 height = img.shape[0]
 width = img.shape[1]
-region_of_interest_vertices = [
-    (0, height), (0, 3*height/5),
-    (width/2, height/2), (width, 3*height/5),
+"""region_of_interest_vertices = [
+    (0, height), (0, 4*height/5),
+    (width/2, height/2), (width, 4*height/5),
     (width, height)
 ]
 def region_of_interest(img, vertices):
@@ -20,7 +21,7 @@ def region_of_interest(img, vertices):
     cv2.fillPoly(mask, vertices, match_mask_color)
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
-img = region_of_interest(img, np.array([region_of_interest_vertices], np.int32))
+img = region_of_interest(img, np.array([region_of_interest_vertices], np.int32))"""
 # Beginning of processing
 kernel = np.ones((5,5),np.uint8)
 opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel) # erode and dilation of the image (opening method)
@@ -28,11 +29,13 @@ skeleton = cv2.subtract(img, opening) # actual image - opened image
 # End of processing
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)#convert to gray scale
 img_edg = cv2.Canny(gray, 100, 100)#detect the edges of the gray image
-hgh_lines = cv2.HoughLinesP(img_edg, 1, np.pi/180, 30, maxLineGap = 2, minLineLength = 50)
+hgh_lines = cv2.HoughLinesP(img_edg, 1, np.pi/180, 30, maxLineGap = 5, minLineLength = 30)
 if hgh_lines is not None:
     for line in hgh_lines:
         x1, y1, x2, y2 = line[0]
-        cv2.line(orig, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        if abs(y1-y2) > 10:
+            #if abs(x1-x2)> 1:
+            cv2.line(orig, (x1, y1), (x2, y2), (0, 255, 0), 3)
 print(hgh_lines)
 cv2.imshow("orig", orig)
 cv2.imshow("Image", img)
--- video_test_gray.py
+++ video_test_gray.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-video = cv2.VideoCapture("Highway4.mp4")
+video = cv2.VideoCapture("Highway5.mp4")
 def region_of_interest(img, vertices):
     mask = np.zeros_like(img)
     channel_count = img.shape[2]
@@ -11,34 +11,36 @@ def region_of_interest(img, vertices):
 
 def lines_detection(img):
     img_orig = img
+    img = cv2.GaussianBlur(img, (5, 5), 0)
     height = img.shape[0]
     width = img.shape[1]
     region_of_interest_vertices = [
-        (width/10, height),
-        (width / 10, height / 2),(width, height/2),
-        (width, height),
+        (width/8, 4*height/5),
+        (width / 8, height / 2),(0.75*width, height/2),
+        (0.75*width, 4*height/5),
     ]
     img = region_of_interest(img, np.array([region_of_interest_vertices], np.int32))
     # Beginning of processing
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((7, 7), np.uint8)
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)  # erode and dilation of the image (opening method)
     skeleton = cv2.subtract(img, opening)  # actual image - opened image
     # End of processing
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert to gray scale
     img_edg = cv2.Canny(gray, 100, 100)  # detect the edges of the gray image
-    hgh_lines = cv2.HoughLinesP(img_edg, 1, np.pi/180, 30, maxLineGap = 15, minLineLength = 200)#maxLineLength = 210)
+    hgh_lines = cv2.HoughLinesP(img_edg, 1, np.pi/180, 30, maxLineGap =1, minLineLength = 30)#maxLineLength = 210)
     print(hgh_lines)
     if hgh_lines is not None:
         for line in hgh_lines:
             x1, y1, x2, y2 = line[0]
-            if y1 != y2:
-                cv2.line(img_orig, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            if abs(x1-x2) > 1:
+                if abs(y1-y2) > 20:
+                    cv2.line(img_orig, (x1, y1), (x2, y2), (0, 255, 0), 5)
     return img_orig, img_edg
 
 while True:
     eov, frame = video.read()# if eov is false then the video has finished
     if eov == False:
-        video = cv2.VideoCapture("Highway4.mp4")
+        video = cv2.VideoCapture("Highway5.mp4")
         eov, frame = video.read()
     frame, edges = lines_detection(frame)
     cv2.imshow("edges", edges)
